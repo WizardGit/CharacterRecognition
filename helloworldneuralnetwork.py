814d21ff68434b589559b1633e8d1f702e6fd13c
Sofi <sofiv@uoregon.edu>
2022-01-26 22:26:56 -0800

    starting digit file

---
+++ helloworldneuralnetwork.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+"""
+torch.nn is used for neural network operations and torch.optim is for neural network optimizers
+1. build a computation graph
+2. set up optimizers
+3. set up criterion
+4. set up data
+5. train the model
+"""
