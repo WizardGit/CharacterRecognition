5d7c3524204e840cbc779420ba3e9cadc36debd8
Sofi <sofiv@uoregon.edu>
2022-01-26 22:38:30 -0800

    working on smaller scale sample neural network

--- digits/helloworldneuralnetwork.py
+++ digits/helloworldneuralnetwork.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 """
+torch contains all PyTorch utilities
 torch.nn is used for neural network operations and torch.optim is for neural network optimizers
 1. build a computation graph
 2. set up optimizers
@@ -10,3 +11,9 @@ torch.nn is used for neural network operations and torch.optim is for neural net
 4. set up data
 5. train the model
 """
+
+# now we define the neural network, training utilities, and the dataset:
+network = nn.Linear(1,1) # we are just creating a straight line here (computation graph)
+optimizer = optim.SGD(network.parameters(), lr=0.1) # setting up the optimizers
+criterion = nn.MSELoss() # setting up the criterion
+x, target = torch.randn((1,)), torch.tensor([0.]) # setting up the data we'll use
